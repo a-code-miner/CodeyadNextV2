131150bc5cfb0d62f2466f290a4d0b721c069889
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-04-05 10:24:47 +0430

    30-page-prerendering-ssg_dynamic_parameters lesson

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,19 @@
+import Link from "next/link";
+import { Container, ListGroup, Row } from "react-bootstrap";
 
 export default function Home(props) {
-	console.log(props);
 	return (
 		<>
-			<ul>
-				{props.posts.map((post) => (
-					<li key={post.id}>{post.title}</li>
-				))}
-			</ul>
+			<Container>
+				<Row className='mt-5'>
+					<ListGroup>
+						<ListGroup.Item action>
+							<Link href={'/users'}><div>users</div></Link>
+						</ListGroup.Item>
+					</ListGroup>
+
+				</Row>
+			</Container>
 		</>
 	);
-};
-
-// This function gets called at build time
-export async function getStaticProps() {
-	// Call an external API endpoint to get posts
-	const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-	const posts = await res.json()
-	// By returning { props: { posts } }, the Blog component
-	// will receive `posts` as a prop at build time
-	return {
-		props: {
-			posts,
-		},
-	};
 };
