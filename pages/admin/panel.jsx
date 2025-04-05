b95369bd415870bf9101c50176a110345b065636
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-04-05 13:55:13 +0430

    34-page-prerendering-swr lesson

---
+++ pages/admin/panel.jsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import useSWR from 'swr';
+
+const handleGetData = async () => {
+    const res = await fetch(`http://localhost:4000/admin`);
+    const resData = await res.json();
+    return resData;
+};
+
+const Admin = () => {
+    const router = useRouter();
+    const { data, isLoading, error, isValidating } = useSWR('admin', handleGetData)
+
+    return isLoading ? (
+        <h1 className='mt-5 text-center'>Loading...</h1>
+    ) : (
+        <div>
+            <Button onClick={() => router.back()}>Back</Button>
+            <ul>
+                {Object.keys(data).map(key => (
+                    <li key={Math.random() * 10}>{`${key} : ${data[key]}`}</li>
+                ))}
+            </ul>
+            {isValidating && (<h1 className='mt-5 text-center'>Loading...</h1>)}
+        </div>
+    );
+};
+
+export default Admin;
--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,14 @@ export default function Home(props) {
 						<ListGroup.Item action>
 							<Link href={'/posts'}><div>posts</div></Link>
 						</ListGroup.Item>
-					</ListGroup>
-					<ListGroup>
 						<ListGroup.Item action>
 							<Link href={'/users'}><div>users</div></Link>
+						</ListGroup.Item>
+						<ListGroup.Item action>
+							<Link href={'/admin'}><div>admin</div></Link>
+						</ListGroup.Item>
+						<ListGroup.Item action>
+							<Link href={'/admin/panel'}><div>admin - panel</div></Link>
 						</ListGroup.Item>
 					</ListGroup>
 				</Row>
